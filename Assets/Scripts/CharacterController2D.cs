3487f7bdaf14ea5f96c488a4ee3904b54c52a604
David Crawford <crawford@mail.gvsu.edu>
2016-11-26 00:56:54 -0500

    Added 2D character controller from https://github.com/cjddmut/Unity-2D-Platformer-Controller.

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+	public Vector2 temp;
+	public float X = 10;
+
+	void Start () {
+		temp = transform.position;
+	}
+
+	void Update () {
+		if(Input.GetKey("left")) {
+			// Gamer pushes left arrow key
+			// Set texture to normal position
+			temp.x += -X * Time.deltaTime;
+		}
+		else if (Input.GetKey("right")) {
+			// Gamer pushes right arrow key
+			// Flip texture
+			temp.x += X * Time.deltaTime;
+		}
+		transform.position = temp;
+	}
+}
--- Assets/Scripts/Globals.cs
+++ Assets/Scripts/Globals.cs
@@ -5,4 +5,28 @@ public static class Globals {
 	public static string username { get; set; }
 	public static string password { get; set; }
 	public static string PlayFabId { get; set; }
+
+	//From character controller
+	// Input threshold in order to take effect. Arbitarily set.
+	public const float INPUT_THRESHOLD = 0.5f;
+	public const float FAST_FALL_THRESHOLD = 0.5f;
+
+	public const int ENV_MASK = 0x100;
+
+	public const string PACKAGE_NAME = "PC2D";
+
+	public const float MINIMUM_DISTANCE_CHECK = 0.01f;
+
+	public static int GetFrameCount(float time)
+	{
+		float frames = time / Time.fixedDeltaTime;
+		int roundedFrames = Mathf.RoundToInt(frames);
+
+		if (Mathf.Approximately(frames, roundedFrames))
+		{
+			return roundedFrames;
+		}
+
+		return Mathf.CeilToInt(frames);
+	}
 }
