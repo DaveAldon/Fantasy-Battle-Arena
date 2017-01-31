feb2ca9d4cb43940897bc21cf3e7da9065750495
David Crawford <DavidAldon@users.noreply.github.com>
2017-01-31 01:49:48 -0500

    Update PlayerController.cs

--- Assets/Scripts/Player Controllers/PlayerController.cs
+++ Assets/Scripts/Player Controllers/PlayerController.cs
@@ -1,26 +1,28 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.Networking;
 
- public class PlayerController : NetworkBehaviour {
- 
-    public float speed = 8.0f; //Walk speed
-    public Animator anim;
+ public class PlayerController : NetworkBehaviour 
+ {
+	public float speed = 8.0f; //Walk speed
+	public Animator anim;
 	private int currentState = 0; //State integer used to determine all animations
 	public bool lastFacedLeft; //Used for determining which idle state to use
-	public int direction = 1; // -1/0/1 values for determining projectile firing direction
-    private bool m_Jump; //Unused until jumping is implemented
-    public RectTransform healthBar;
+    	public int direction = 1; // -1/0/1 values for determining projectile firing direction
+    	private bool m_Jump; //Unused until jumping is implemented
+    	public RectTransform healthBar;
 
-    void Start() {
-
-        anim = gameObject.GetComponent<Animator> ();
+    	//Called in order after the Awake method on initialization
+    	void Start() 
+    	{
+        	anim = gameObject.GetComponent<Animator> ();
 
 		if (isLocalPlayer) { //if I am the owner of this prefab
 			Camera.main.GetComponent<CameraFollow>().target = transform;
-    	}
-     }
+    		}
+     	}
 
-	private void Update()
+	//Called every frame
+	private void Update() 
 	{
 		if (isLocalPlayer)
 		{
@@ -36,7 +38,9 @@ using UnityEngine.Networking;
 		}
 	}
 
-	private void FixedUpdate() {
+	//Called every cycle, ignoring framerate
+	private void FixedUpdate() 
+	{
 		if(isLocalPlayer) {
 			if(Input.GetKey(KeyCode.A)) {
 				direction = -1;
@@ -82,6 +86,7 @@ using UnityEngine.Networking;
 		RpcMove(dir, jump);
 	}
 
+	//Only the server can command clients to run this method
 	[ClientRpc]
 	void RpcMove(int dir, bool jump)
 	{
@@ -91,8 +96,8 @@ using UnityEngine.Networking;
 		Move(dir, jump);
 	}
 
-	//After the server updates all of the clients (including us), then our transform is updated
-	public void Move(int dir, bool jump)
+	//After the server updates all of the clients, then our transform is updated
+	void Move(int dir, bool jump)
 	{
 		Vector2 newScale = transform.localScale;
                 newScale.x = 1.0f;
@@ -102,7 +107,7 @@ using UnityEngine.Networking;
 
 	//Runs the animation state handler on all clients
 	[Command]
-	public void CmdSpriteChange(int sprite)
+	void CmdSpriteChange(int sprite)
 	{
 		RpcSpriteChange(sprite);
 	}
